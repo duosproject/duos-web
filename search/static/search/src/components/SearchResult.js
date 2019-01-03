81a6dc5c740170c972c862bd2bccbeccf09f6060
Ainsley McGrath <mcgrath.ainsley@gmail.com>
2019-01-03 14:01:07 -0600

    search results on frontend
    Signed-off-by: Ainsley McGrath <mcgrath.ainsley@gmail.com>

--- search/static/search/src/components/SearchResult.js
+++ search/static/search/src/components/SearchResult.js
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function SearchResult(props) {
-  return <div>{JSON.stringify(props.data)}</div>;
+  return (
+    <div className="container content">
+      <h3 className="is-size-6">
+        {props.data.articleName} by {props.data.authorName}
+      </h3>
+      <p>{props.data.datasetName}</p>
+      <br />
+    </div>
+  );
 }
