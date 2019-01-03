82e7a46e8caad84621910a6a3dfc075de21c7446
Ainsley McGrath <mcgrath.ainsley@gmail.com>
2019-01-03 14:12:22 -0600

    redirect base index to org page
    Signed-off-by: Ainsley McGrath <mcgrath.ainsley@gmail.com>

--- duos_research/urls.py
+++ duos_research/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include
 from django.urls import path
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path("", lambda request: redirect("https://duosproject.github.io//")),
     path("feedback/", include("feedback.urls")),
     path("search/", include("search.urls")),
 ]
