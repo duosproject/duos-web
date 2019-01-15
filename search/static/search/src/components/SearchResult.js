f4a00b65a1b7d5920a134c5e7bdb91ec4903eeaf
Ainsley McGrath <mcgrath.ainsley@gmail.com>
2019-01-14 20:21:34 -0600

    list of authors / datasets on frontend
    Signed-off-by: Ainsley McGrath <mcgrath.ainsley@gmail.com>

--- search/static/search/src/components/SearchResult.js
+++ search/static/search/src/components/SearchResult.js
@@ -3,10 +3,19 @@ import React from "react";
 export default function SearchResult(props) {
   return (
     <div className="container content">
-      <h3 className="is-size-6">
-        {props.data.articleName} by {props.data.authorName}
-      </h3>
-      <p>{props.data.datasetName}</p>
+      <h3 className="is-size-6">{props.data.articleName}</h3>
+      Authors:
+      <ul>
+        {props.data.authorName.map(a => (
+          <li>{a}</li>
+        ))}
+      </ul>
+      Datasets:
+      <ul>
+        {props.data.datasetName.map(a => (
+          <li>{a}</li>
+        ))}
+      </ul>
       <br />
     </div>
   );
