97bee849a9cfafe93fc420c3ef79ec4fc1857bf5
Ainsley McGrath <mcgrath.ainsley@gmail.com>
2019-01-14 20:12:27 -0600

    aggregate search results; loathe self & whoosh
    Signed-off-by: Ainsley McGrath <mcgrath.ainsley@gmail.com>

--- search/views.py
+++ search/views.py
@@ -11,23 +11,48 @@ def index(request):
     if request.GET.get("q", ""):
         q = Query()
 
-        search_results = q.search_from_user_query(request.GET.get("q", ""))
+        # its a generator because the whoosh API is irritating. TODO: destroy whoosh
+        _search_results = q.search_from_user_query(request.GET.get("q", ""))
+        search_results = []
 
-        try:
-            return JsonResponse(
+        # calling list() on the generator throws an exception TODO: destroy whoosh
+        for result in _search_results:
+            search_results.append(
+                # appending `result` explicitly to make a copy throws an exception: TODO: destroy whoosh
                 {
-                    "resultList": [
-                        {
-                            # make it javascripty
-                            "articleName": result["article_title"],
-                            "authorName": result["author_name"],
-                            "datasetName": result["dataset_name"],
-                        }
-                        for result in search_results
-                    ]
+                    "author_name": result["author_name"],
+                    "article_title": result["article_title"],
+                    "dataset_name": result["dataset_name"],
                 }
             )
-        finally:
-            q.close()
+
+        distinct_articles = {r["article_title"] for r in search_results}
+        json_result_list = []
+
+        for article in distinct_articles:
+            if article not in [j["articleName"] for j in json_result_list]:
+                json_result_list.append(
+                    {
+                        "articleName": article,
+                        "authorName": list(  # sets are not serializable
+                            {
+                                r["author_name"]
+                                for r in search_results
+                                if r["article_title"] == article
+                            }
+                        ),
+                        "datasetName": list(
+                            {
+                                r["dataset_name"]
+                                for r in search_results
+                                if r["article_title"] == article
+                            }
+                        ),
+                    }
+                )
+
+        q.close()
+
+        return JsonResponse({"resultList": json_result_list})
 
     return render(request, "search/index.html")
