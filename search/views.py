14a89739212a05b55c785fc59f2d6ec06b60a0cb
Ainsley McGrath <mcgrath.ainsley@gmail.com>
2019-02-25 15:32:48 -0600

    add basic logging of user seach queries
    Signed-off-by: Ainsley McGrath <mcgrath.ainsley@gmail.com>

--- search/views.py
+++ search/views.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-s
+import logging
+import sys
+
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
@@ -8,12 +11,22 @@ from search.db.query import Query
 @require_http_methods(["GET"])
 def index(request):
 
+    logging.basicConfig(
+        stream=sys.stdout,
+        level=logging.INFO,
+        datefmt="%m/%d/%Y %H:%M:%S",
+        format="%(asctime)s %(message)s",
+    )
+
     if request.GET.get("q", ""):
         q = Query()
 
         search_results = q.search_from_user_query(request.GET.get("q", ""))
         q.close()
 
+        logging.info(
+            f"""User originating from {request.META["REMOTE_ADDR"]} searched for: {request.GET.get("q", "")}"""
+        )
         return JsonResponse(
             {
                 "resultList": [
